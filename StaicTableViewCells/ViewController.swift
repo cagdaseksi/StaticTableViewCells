386679668f7f06fbba30b3142fed5952019e61ef
cagdaseksi <cagdas.eksi@hotmail.com>
2019-01-12 13:37:20 +0300

    Initial Commit

---
+++ StaicTableViewCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StaicTableViewCells
+//
+//  Created by MAC on 12/1/19.
+//  Copyright © 2019 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
